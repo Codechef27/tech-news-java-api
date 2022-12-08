95386a0ea30efe82a4dcb2f42564fdfe1a8b2da3
BradleyK <bradleyjkennett@gmail.com>
2022-12-08 15:06:36 -0500

    java version change

--- src/main/java/com/technews/controller/PostController.java
+++ src/main/java/com/technews/controller/PostController.java
@@ -6,6 +6,7 @@ import com.technews.model.Vote;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
+//import javax.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
